7e1a8bb67cf1da26ab604576e990593a72f6ccb0
Asami Doi <asamidoi@chromium.org>
2024-08-23 15:23:04 +0200

    Initial Commit

---
+++ GemmaOnCoreML/GemmaOnCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  GemmaOnCoreMLApp.swift
+//  GemmaOnCoreML
+//
+//  Created by Asami Doi on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GemmaOnCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
